63336bff3facde1bb92319633d1c08a1c529ac0b
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2024-02-15 13:39:49 +0100

    add login route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,10 @@ import "dotenv/config";
 
 import express, { Request, Response } from "express";
 
+import authRoutes from './routes/auth/login'
 import cors from "cors";
 import mongoose from "mongoose";
-import userRoutes from "./routes/users";
+import userRoutes from "./routes/auth/register";
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
 
@@ -14,12 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/auth", authRoutes);
 app.get("/api/v1/test", async (req: Request, res: Response) => {
   res.json({
     message: "Welcome to the STUDENTS HOSTEL BOOKING APP API!!!",
   });
 });
-
 
 const PORT = 4000;
 
